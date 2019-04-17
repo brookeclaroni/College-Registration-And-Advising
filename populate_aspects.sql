4373acf6f2e6d95866f008238c5b1d855e22b259
brookeclaroni <32200125+brookeclaroni@users.noreply.github.com>
2019-04-17 19:27:30 -0400

    Update populate_aspects.sql

--- populate_aspects.sql
+++ populate_aspects.sql
@@ -1,1 +1,33 @@
+create table aspects(
+  id int,
+  advisorid int,
+  startYear VARCHAR(16),
+  gradYear VARCHAR(16),
+  -- role varchar(100),
+  reviewForm int,
+  approveThesis int,
+  clearedToGrad int,
+  primary key(id),
+  foreign key(id) references user(uid)
+);
 
+
+INSERT INTO aspects VALUES (
+    88888888, 
+    NULL,
+    2017, 
+    2021,
+    0, 
+    0,
+    0
+);
+
+INSERT INTO aspects VALUES (
+    99999999,
+    NULL,
+    2017, 
+    2021,
+    0, 
+    0,
+    0
+);
