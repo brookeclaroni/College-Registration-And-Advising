0e0bc807209796dc352ea1aae24fc55bad4a1d2f
brookeclaroni <32200125+brookeclaroni@users.noreply.github.com>
2019-04-17 13:51:23 -0400

    update navbar for alum

--- public_html/navbar.php
+++ public_html/navbar.php
@@ -9,6 +9,10 @@
             echo ("<a href=\"transcript.php\">Transcript</a>");
             echo ("<a href=\"gradeCourses.php\">Grades</a>");
         }
+        if (in_array("alumni", $_SESSION["user_role"])) {
+            echo ("<a href=\"courses.php\">Courses</a>");
+            echo ("<a href=\"transcript.php\">Transcript</a>");
+        }
         if (in_array("instructor", $_SESSION["user_role"])) {
             echo ("<a href=\"courses.php\">Courses</a>");
             echo ("<a href=\"gradeCourses.php\">Grades</a>");
