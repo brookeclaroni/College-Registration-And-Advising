c6ce0746013111c929bdf3898d8bf31a5a8dfa87
brookeclaroni <32200125+brookeclaroni@users.noreply.github.com>
2019-04-17 14:18:39 -0400

    Create viewForm1.php

---
+++ public_html/viewForm1.php
@@ -0,0 +1,64 @@
+<?php
+session_start();
+if (empty($_SESSION["user_id"])) {
+    header("Location: index.php");
+}
+if (
+    !in_array("advisor", $_SESSION["user_role"])
+) {
+    header("Location: index.php");
+}
+$uid = $_POST["uid"];
+$servername = "127.0.0.1";
+$username = "harmonandbrooke";
+$password = "DBteam18!";
+$dbname = "harmonandbrooke";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    // Redirect to user friendly error page
+    die('Error: ' . mysqli_connect_error());
+}
+?>
+
+<!DOCTYPE html>
+
+<head>
+    <title>Enroll</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <?php
+    include "navbar.php";
+    ?>
+    <div class="main-container">
+        <h1>Form 1</h1>
+        <?php
+    // Show only form 1 inputs from the student selected
+        $query = "SELECT * FROM formOne, schedule, course WHERE schedule.sid=enrolls.sid AND course.cid=schedule.cid AND formOne.uid=" . $uid . "";
+        $result = mysqli_query($conn, $query);
+        // Generate a table of all the classes listed in form 1
+        if (mysqli_num_rows($result) > 0) {
+            echo "<table>
+                <tr>
+                    <th>Course</th>
+                    <th>Title</th>
+                </tr>";
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>";
+                echo "<td><a href=\"transcriptCourse.php?cid=" . $row["cid"] . "\">" . $row["dept"] . " " . $row["cnum"] . "</a></th>";
+                echo "<td>" . $row["title"] . "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+        }
+        ?>
+    </div>
+</body>
+
+</html> 
