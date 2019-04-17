887a9d103410b8fd63804d05410373c5371f9ec1
brookeclaroni <32200125+brookeclaroni@users.noreply.github.com>
2019-04-17 15:17:08 -0400

    Update manageAdvisees.php

--- public_html/manageAdvisees.php
+++ public_html/manageAdvisees.php
@@ -1,5 +1,76 @@
-<html>
+<?php
+session_start();
+if (empty($_SESSION["user_id"])) {
+    header("Location: index.php");
+}
+if (
+    !in_array("advisor", $_SESSION["user_role"])
+) {
+    header("Location: index.php");
+}
+$uid = $_SESSION["user_id"];
+$servername = "127.0.0.1";
+$username = "harmonandbrooke";
+$password = "DBteam18!";
+$dbname = "harmonandbrooke";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    // Redirect to user friendly error page
+    die('Error: ' . mysqli_connect_error());
+}
+function trim_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
+
+<!DOCTYPE html>
+
+<head>
+    <title>Manage Advisees</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
 <body>
-Manage Advisees
+    <?php
+    include "navbar.php";
+    ?>
+    <div class="main-container">
+        if (in_array("admin", $_SESSION["user_role"]) || in_array("gs", $_SESSION["user_role"])) {
+            echo "<h1>Advisee List</h1>";
+            $query = "SELECT * FROM user INNER JOIN aspects ON user.uid = aspects.uid WHERE aspect.advisorid =".$uid."";
+            $result = mysqli_query($conn, $query);
+            if (mysqli_num_rows($result) > 0) {
+                echo "<table>
+                    <tr>
+                        <th>User Name</th>
+                        <th>User ID</th>
+                        <th>Address</th>
+                        <th>Transcript</th>
+                        <th>Form 1</th>
+                    </tr>";
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<tr>";
+                    echo "<td>" . $row["fname"] . " " . $row["lname"] . " " . "</td>";
+                    echo "<td>" . $row["uid"] . "</td>";
+                    echo "<td>" . $row["address"] . "</td>";
+                    echo "<td>View</td>";
+                    echo "<td>View</td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+            }
+        }
+        ?>
+    </div>
 </body>
-</html>
+
+</html> 
