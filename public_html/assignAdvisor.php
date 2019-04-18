7f8a087e9565271e7a9275e973fbdf6026e74f40
brookeclaroni <32200125+brookeclaroni@users.noreply.github.com>
2019-04-17 20:17:56 -0400

    Create assignAdvisor.php

---
+++ public_html/assignAdvisor.php
@@ -0,0 +1,33 @@
+<?php
+
+	session_start();
+if (empty($_SESSION["user_id"])) {
+    header("Location: index.php");
+}
+if (
+    !in_array("gs", $_SESSION["user_role"])
+) {
+    header("Location: index.php");
+}
+$uid = $_SESSION["user_id"];
+$servername = "127.0.0.1";
+$username = "harmonandbrooke";
+$password = "DBteam18!";
+$dbname = "harmonandbrooke";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    // Redirect to user friendly error page
+    die('Error: ' . mysqli_connect_error());
+}
+
+		$studentid = $_POST["id"];
+		$advisorid = $_POST["advisorid"];
+		
+		
+		$assign_query = "UPDATE aspects SET advisorid = ".$advisorid." WHERE id = ".$studentid."";
+		if(mysqli_query($conn,$assign_query))
+		{
+			header("Location: manageAdvising.php");
+		}
+?>
