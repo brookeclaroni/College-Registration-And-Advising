bb5ee96261994669cb58cf85b45e4772319e53eb
brookeclaroni <32200125+brookeclaroni@users.noreply.github.com>
2019-04-17 21:18:41 -0400

    Update manageAdvisees.php

--- public_html/manageAdvisees.php
+++ public_html/manageAdvisees.php
@@ -61,7 +61,9 @@ function trim_input($data)
                     echo "<td>" . $row["fname"] . " " . $row["lname"] . " " . "</td>";
                     echo "<td>" . $row["uid"] . "</td>";
                     echo "<td>" . $row["address"] . "</td>";
-                    echo "<td>View</td>";
+                    echo "<td>";
+                    echo '<form action="transcript.php" method="post"><button type="submit">View</button></form>';
+                    echo "</td>";
                     echo "<td>View</td>";
                     echo "</tr>";
                 }
