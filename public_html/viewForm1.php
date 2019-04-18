e9260d73b133e5b8d15a9c8d9c8e1113e2cc6154
brookeclaroni <32200125+brookeclaroni@users.noreply.github.com>
2019-04-18 17:54:18 -0400

    Update viewForm1.php

--- public_html/viewForm1.php
+++ public_html/viewForm1.php
@@ -44,7 +44,7 @@ if (!$conn) {
         <h1>Form 1</h1>
         <?php
     // Show only form 1 inputs from the student selected
-        $query = "SELECT * FROM formOne WHERE formOne.id=" . $uid . "";
+        $query = "SELECT * FROM formOne f, course c WHERE f.id=" . $uid . " AND f.dept = c.dept AND f.courseNumber = c.cnum";
         $result = mysqli_query($conn, $query);
         // Generate a table of all the classes listed in form 1
         if (mysqli_num_rows($result) > 0) {
@@ -55,7 +55,7 @@ if (!$conn) {
                 </tr>";
             while ($row = mysqli_fetch_assoc($result)) {
                 echo "<tr>";
-                echo "<td><a href=\"transcriptCourse.php?cid=" . $row["cid"] . "\">" . $row["dept"] . " " . $row["cnum"] . "</a></th>";
+                echo "<td>" . $row["dept"] . " " . $row["courseNumber"] . "</th>";
                 echo "<td>" . $row["title"] . "</td>";
                 echo "</tr>";
             }
