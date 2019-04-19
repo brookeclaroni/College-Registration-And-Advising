808b4065d8a626251aff7b3eb3c63ff174ad5d3d
brookeclaroni <32200125+brookeclaroni@users.noreply.github.com>
2019-04-18 23:09:17 -0400

    Update graduateValidate.php

--- public_html/graduateValidate.php
+++ public_html/graduateValidate.php
@@ -1,1 +1,140 @@
+<?php
+session_start();
+if (empty($_SESSION["user_id"])) {
+    header("Location: index.php");
+}
+if (
+    !in_array("student", $_SESSION["user_role"])
+) {
+    header("Location: index.php");
+}
+if (!empty($_POST["id"])) {
+    $id = $_POST["id"];    
+} else {  
+    $id = $_SESSION["user_id"];
+}
+$servername = "127.0.0.1";
+$username = "harmonandbrooke";
+$password = "DBteam18!";
+$dbname = "harmonandbrooke";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    // Redirect to user friendly error page
+    die('Error: ' . mysqli_connect_error());
+}
+?>
 
+<!DOCTYPE html>
+
+<head>
+    <title>Apply to Graduate</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<?php
+      $id = $_POST['id'];
+      $deptArray = array();
+      $numArray = array();
+      $gradeArray = array();
+      $creditArray = array();
+      $compBool = 0;
+      // Check connection
+      if(!$conn){
+        die("Connection failed: " . mysqli_connect_error());
+      }
+      // check if classes taken are equivalent to form1
+      $x = 0;
+      $query = "SELECT * FROM formOne WHERE id = '$id'";
+      $result = mysqli_query($conn, $query) or die("Bad Query: $query");
+      while($row = mysqli_fetch_array($result)) {
+          $deptArray[$x] = $row['dept'];
+          $numArray[$x] = $row['courseNumber'];
+          $x++;
+      }
+      $numformOne = mysqli_num_rows($result);
+      $y = 0;
+      $queryTaken = "SELECT * FROM enrolls e, schedule s, course c WHERE e.uid = '$id' AND e.sid=s.sid AND s.cid = c.cid";
+      $resultTaken = mysqli_query($conn, $queryTaken) or die("Bad Query: $queryTaken");
+      while($row = mysqli_fetch_array($resultTaken)){
+        $deptTaken[$y] = $row['dept'];
+        $numTaken[$y] = $row['cnum'];
+        $y++;
+      }
+      sort($deptArray);
+      sort($numArray);
+      sort($deptTaken);
+      sort($numTaken);
+      if($deptArray == $deptTaken && $numArray == $numTaken){
+        $compBool = 1;
+      }
+      for($x = 0; $x < 12; $x++){
+        $query2 = "SELECT * FROM enrolls
+                    WHERE dept = '$deptArray[$x]'
+                      AND cnum = '$numArray[$x]'";
+        $result2 = mysqli_query($conn, $query2) or die("Bad Query: $query2");
+        while($row = mysqli_fetch_array($result2)){
+          $gradeArray[$x] = $row['grade'];
+          $creditArray[$x] = $row['creditHours'];
+        }
+      }
+      $creditCount = 0;
+      for($x = 0; $x < 12; $x++){
+        $creditCount = $creditCount + $creditArray[$x];
+      }
+        $failCounter = 0;
+        $totalGPA = 0.0;
+        $error = 0;
+        for($x = 0; $x < 12; $x++){
+          if($gradeArray[$x] != "A" && $gradeArray[$x] != "A-" && $gradeArray[$x] != "B+" && $gradeArray[$x] != "B"){
+            $failCounter++;
+          }
+          if($gradeArray[$x] == "A"){
+            $totalGPA = $totalGPA + (4.0 * $creditArray[$x]);
+          }
+          if($gradeArray[$x] == "A-"){
+            $totalGPA = $totalGPA + (3.7 * $creditArray[$x]);
+          }
+          if($gradeArray[$x] == "B+"){
+            $totalGPA = $totalGPA + (3.3 * $creditArray[$x]);
+          }
+          if($gradeArray[$x] == "B"){
+            $totalGPA = $totalGPA + (3.0 * $creditArray[$x]);
+          }
+          if($gradeArray[$x] == "B-"){
+            $totalGPA = $totalGPA + (2.7 * $creditArray[$x]);
+          }
+          if($gradeArray[$x] == "C+"){
+            $totalGPA = $totalGPA + (2.3 * $creditArray[$x]);
+          }
+          if($gradeArray[$x] == "C"){
+            $totalGPA = $totalGPA + (2.0 * $creditArray[$x]);
+          }
+          if($gradeArray[$x] == "IP"){
+            $error = 1;
+          }
+        }
+        $numClasses = 0;
+        $query3 = "SELECT * FROM enrolls WHERE uid = '$id'";
+        $result3 = mysqli_query($conn, $query3) or die("Bad Query: $query3");
+        $numClasses = mysqli_num_rows($result3);
+        $totalGPA = $totalGPA / $creditCount;
+        if($error != 1 && $totalGPA >= 3.0 && $failCounter <= 2 && $compBool == 1){
+          $query4 = "UPDATE aspects SET clearedToGrad = 1 WHERE uid = '$id'";
+          $result4 = mysqli_query($conn, $query4) or die("Bad Query: $query4");
+          header("Location: applyToGraduate.php");
+        }
+        else{
+          header("Location: applyToGraduate.php");
+        }
+      //close connection
+        mysqli_close($conn);
+    ?>
+      </div>
+</body>
+</html> 
