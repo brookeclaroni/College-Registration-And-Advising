727f3ce80d940d978a6eff5f7cf172974a93e2e3
brookeclaroni <32200125+brookeclaroni@users.noreply.github.com>
2019-04-20 01:10:22 -0400

    Update thesis.php

--- public_html/thesis.php
+++ public_html/thesis.php
@@ -3,7 +3,7 @@ session_start();
 if (empty($_SESSION["user_id"])) {
     header("Location: index.php");
 }
-else if ($_SESSION["user_role"] != "student")
+else if (!in_array("student", $_SESSION["user_role"]))
 {
   header("Location: index.php");
 }
