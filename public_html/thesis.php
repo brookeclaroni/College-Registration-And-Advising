31de77c7380a650eaddc89f0eb3ea577f16f3d55
brookeclaroni <32200125+brookeclaroni@users.noreply.github.com>
2019-04-20 18:40:32 -0400

    Update thesis.php

--- public_html/thesis.php
+++ public_html/thesis.php
@@ -27,7 +27,7 @@ $temp = explode(".", $_FILES["pdf_file"]["name"]);
 $extension = end($temp);
 $upload_pdf=$_FILES["pdf_file"]["name"];
 move_uploaded_file($_FILES["pdf_file"]["tmp_name"],"uploads/pdf/" . $_FILES["pdf_file"]["name"]);
-$sql=mysqli_query($con,"INSERT INTO thesis (data)VALUES($upload_pdf)");
+$sql=mysqli_query($con,"INSERT INTO thesis (uid, data)VALUES($uid, $upload_pdf)");
 if($sql){
 	echo "Data Submit Successful";
 	header("Location: viewThesis.php");
