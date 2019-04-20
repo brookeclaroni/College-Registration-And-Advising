70fd8752f3d9f739988dd105086350ce98c8e626
brookeclaroni <32200125+brookeclaroni@users.noreply.github.com>
2019-04-20 19:12:00 -0400

    Update viewThesis.php

--- public_html/viewThesis.php
+++ public_html/viewThesis.php
@@ -44,19 +44,14 @@ if (!$conn) {
 //     move_uploaded_file($_FILES["pdf_file"]["tmp_name"],"uploads/pdf/" . $_FILES["pdf_file"]["name"]);
 //     $sql=mysqli_query($con,"INSERT INTO thesis (uid, pdf)VALUES('$uid', '$upload_pdf')");
     
-//     $pdf_file = $_POST['pdf_file'];
-//     $filePointer = fopen($_FILES[$pdf_file]['name'], 'r');
-//     $fileData = fread($filePointer, filesize($_FILES['pdf_file']['name']));
-//     $fileData = addslashes($fileData);
-//     $pdfQuery = "INSERT INTO thesis (uid, data) VALUES( '$uid', '$fileData' )";
-//     if(mysqli_query($conn,$pdfQuery))
-// 		{
-// 			echo "Your thesis was successfully uploaded!";
-// 		}
-// 		else
-// 		{
-// 			echo "There was an issue with uploading your thesis.";
-// 		}
+    $filePointer = fopen($_FILES[$pdf_file]['name'], 'r');
+    $fileData = fread($filePointer, filesize($_FILES['pdf_file']['name']));
+    $fileData = addslashes($fileData);
+    $pdfQuery = "INSERT INTO thesis (uid, data) VALUES( '$uid', '$fileData' )";
+    if(mysqli_query($conn,$pdfQuery))
+		{
+			echo "Your thesis was successfully uploaded!";
+		}
 	
 	
 	
