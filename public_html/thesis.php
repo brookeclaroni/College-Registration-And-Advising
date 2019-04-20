ff51beb25695a8010b2b068198a7f4f1e2161fd7
brookeclaroni <32200125+brookeclaroni@users.noreply.github.com>
2019-04-20 01:08:50 -0400

    Update thesis.php

--- public_html/thesis.php
+++ public_html/thesis.php
@@ -1,7 +1,47 @@
-<html>
+<?php
+session_start();
+if (empty($_SESSION["user_id"])) {
+    header("Location: index.php");
+}
+else if ($_SESSION["user_role"] != "student")
+{
+  header("Location: index.php");
+}
+$uid = $_SESSION["user_id"];
+
+$servername = "127.0.0.1";
+$username = "harmonandbrooke";
+$password = "DBteam18!";
+$dbname = "harmonandbrooke";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    // Redirect to user friendly error page
+    die('Error: ' . mysqli_connect_error());
+}
+?>
+
+<!DOCTYPE html>
+
+<head>
+    <title>Thesis</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
 <body>
+    <?php
+    include "navbar.php";
+    ?>
+    <div class="main-container">
+        <h1>Thesis</h1>
 <textarea rows="45" cols="125">
 Paste your thesis here as plain text.
 </textarea>
+    </div>
 </body>
-</html>
+
+</html> 
