bba0e9999a4daffb9533e6ca07cc3e01810d7670
brookeclaroni <32200125+brookeclaroni@users.noreply.github.com>
2019-04-20 18:16:18 -0400

    Update viewThesis.php

--- public_html/viewThesis.php
+++ public_html/viewThesis.php
@@ -45,18 +45,18 @@ if (!$conn) {
 //     $sql=mysqli_query($con,"INSERT INTO thesis (uid, pdf)VALUES('$uid', '$upload_pdf')");
     
     
-//     $filePointer = fopen($_FILES['pdf_file']['name'], 'r');
-//     $fileData = fread($filePointer, filesize($_FILES['pdf_file']['name']));
-//     $fileData = addslashes($fileData);
-//     $pdfQuery = "INSERT INTO thesis (uid, data) VALUES( '$uid', '$fileData' )";
-//     if(mysqli_query($conn,$pdfQuery))
-// 		{
-// 			echo "Your thesis was successfully uploaded!";
-// 		}
-// 		else
-// 		{
-// 			echo "There was an issue with uploading your thesis.";
-// 		}
+    $filePointer = fopen($_FILES['pdf_file']['name'], 'r');
+    $fileData = fread($filePointer, filesize($_FILES['pdf_file']['name']));
+    $fileData = addslashes($fileData);
+    $pdfQuery = "INSERT INTO thesis (uid, data) VALUES( '$uid', '$fileData' )";
+    if(mysqli_query($conn,$pdfQuery))
+		{
+			echo "Your thesis was successfully uploaded!";
+		}
+		else
+		{
+			echo "There was an issue with uploading your thesis.";
+		}
 	
 	
 	
