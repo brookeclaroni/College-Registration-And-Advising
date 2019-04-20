a97408676273ab6408313eba11bcef2339457354
brookeclaroni <32200125+brookeclaroni@users.noreply.github.com>
2019-04-20 01:22:02 -0400

    Create viewThesis.php

---
+++ public_html/viewThesis.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+if (empty($_SESSION["user_id"])) {
+    header("Location: index.php");
+}
+else if (!in_array("student", $_SESSION["user_role"]))
+{
+  header("Location: index.php");
+}
+$uid = $_SESSION["user_id"];
+$servername = "127.0.0.1";
+$username = "harmonandbrooke";
+$password = "DBteam18!";
+$dbname = "harmonandbrooke";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    // Redirect to user friendly error page
+    die('Error: ' . mysqli_connect_error());
+}
+?>
+
+<!DOCTYPE html>
+
+<head>
+    <title>Thesis</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <?php
+    include "navbar.php";
+    echo '<h1>Thesis</h1>';
+    echo '<div class="main-container">'
+    
+    echo $_POST["thesisText"];
+    ?>
+   
+       
+    </div>
+</body>
+
+</html> 
