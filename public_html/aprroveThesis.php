5f53457a2bfdbe67bd291b6d06a2ad17fd9bf9c3
brookeclaroni <32200125+brookeclaroni@users.noreply.github.com>
2019-05-03 17:48:28 -0400

    Update aprroveThesis.php

--- public_html/aprroveThesis.php
+++ public_html/aprroveThesis.php
@@ -1,1 +1,32 @@
-
+<?php
+	session_start();
+if (empty($_SESSION["user_id"])) {
+    header("Location: index.php");
+}
+if (
+    !in_array("gs", $_SESSION["user_role"])
+) {
+    header("Location: index.php");
+}
+$uid = $_POST["uid"];
+$servername = "127.0.0.1";
+$username = "harmonandbrooke";
+$password = "DBteam18!";
+$dbname = "harmonandbrooke";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    // Redirect to user friendly error page
+    die('Error: ' . mysqli_connect_error());
+}
+				
+		$approve_query = "UPDATE aspects SET aprroveThesis = 1 WHERE id = ".$uid."";
+		if(mysqli_query($conn,$approve_query))
+		{
+			header('Location: clearedToGrad.php');
+		}
+		else
+		{
+			echo "error";
+		}
+?>
