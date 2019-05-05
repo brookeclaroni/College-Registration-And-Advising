b4341fb7cc9329c2467b9291ca298730e077b1ea
brookeclaroni <32200125+brookeclaroni@users.noreply.github.com>
2019-05-05 18:47:55 -0400

    Update navbar.php

--- public_html/navbar.php
+++ public_html/navbar.php
@@ -1,5 +1,5 @@
-<div class="navbar">
 <link rel="icon" href="http://www.gwrha.com/uploads/1/7/9/9/17997469/gw_atx_4cp_pos.png">
+<div class="navbar">
     <a href="index.php">Home</a>
     <?php
     if (empty($_SESSION["user_id"])) {
